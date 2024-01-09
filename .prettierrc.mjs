955e501f7a2c5b0232db4d35d956e66d95a754e6
cristianqsanchez <cristianquintero316@gmail.com>
2024-01-08 23:33:15 -0500

    chore: :tada: re-start astro project with bun

---
+++ .prettierrc.mjs
@@ -0,0 +1,20 @@
+// .prettierrc.mjs
+/** @type {import("prettier").Config} */
+export default {
+  bracketSameLine: true,
+  bracketSpacing: true,
+  singleQuote: true,
+  jsxSingleQuote: true,
+  printWidth: 80,
+  htmlWhitespaceSensitivity: true,
+  proseWrap: "always",
+  plugins: ['prettier-plugin-astro'],
+  overrides: [
+    {
+      files: '*.astro',
+      options: {
+        parser: 'astro',
+      },
+    },
+  ],
+};
